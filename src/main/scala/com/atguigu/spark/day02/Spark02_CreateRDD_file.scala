c3ca7acf407b525e794a86501181891a26c9ddda
yanggit2019 <838067203@qq.com>
2020-09-05 19:33:52 +0800

    从本地文件读取数据，创建rdd

---
+++ src/main/scala/com/atguigu/spark/day02/Spark02_CreateRDD_file.scala
@@ -0,0 +1,19 @@
+package com.atguigu.spark.day02
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+object Spark02_CreateRDD_file {
+  def main(args: Array[String]): Unit = {
+    //2.创建Spark配置文件对象
+    val conf: SparkConf = new SparkConf().setMaster("local[*]").setAppName("Spark01_CreateRDD_mem")
+    //1.创建SparkContext对象
+    val sc = new SparkContext(conf)
+
+    //3.从本地文件读取数据，创建rdd
+    val rdd: RDD[String] = sc.textFile("H:\\spark-0105\\input")
+    rdd.collect().foreach(println)
+    //关闭连接
+    sc.stop()
+  }
+}

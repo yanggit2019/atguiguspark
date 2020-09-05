dcdb0ee34018804b5039d8769aafb53661fa9695
yanggit2019 <838067203@qq.com>
2020-09-06 00:26:44 +0800

    转换算子mapPartition 高级版 添加注释

--- src/main/scala/com/atguigu/spark/day03/Spark01_Transformation_mapPartitions.scala
+++ src/main/scala/com/atguigu/spark/day03/Spark01_Transformation_mapPartitions.scala
@@ -11,6 +11,8 @@ object Spark01_Transformation_mapPartitions {
     val sc = new SparkContext(conf)
     val rdd: RDD[Int] = sc.makeRDD(List(1, 2, 3, 4), 2)
     //以分区为单位，对Rdd中的元素进行映射
+    //一般适用于批处理的操作，比如：将Rdd中的元素插入到数据库中，需要数据的连接，如果每个元素都创建一个连接，可以对每个分区的元素
+    //创建一个连接
     val newRdd: RDD[Int] = rdd.mapPartitions(
       _.map(_ * 2)
     )

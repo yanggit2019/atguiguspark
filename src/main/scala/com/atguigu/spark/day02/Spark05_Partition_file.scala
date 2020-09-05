c8b3bf46d763bef090243b81d9e7a49ead9df50b
yanggit2019 <838067203@qq.com>
2020-09-05 21:31:06 +0800

    从本地文件读取数据创建rdd,输入数据指定分区，最终分区数为4

--- src/main/scala/com/atguigu/spark/day02/Spark05_Partition_file.scala
+++ src/main/scala/com/atguigu/spark/day02/Spark05_Partition_file.scala
@@ -12,10 +12,12 @@ object Spark05_Partition_file {
     val sc = new SparkContext(conf)
 
     //3.从本地文件读取数据，创建rdd
-    //输入数据1 2 3 4 采用默认分区方式，最终分区数
-    val rdd: RDD[String] = sc.textFile("H:\\spark-0105\\input\\2.txt")
+    //输入数据1 2 3 4 采用默认分区方式，最终分区数2
+//    val rdd: RDD[String] = sc.textFile("H:\\spark-0105\\input\\2.txt")
+    //输入数据1 2 3 4 指定分区数，minPartitions设置为3，最终分区数为4
+    val rdd: RDD[String] = sc.textFile("H:\\spark-0105\\input\\2.txt",3)
     rdd.saveAsTextFile("H:\\spark-0105\\output")
-    rdd.collect().foreach(println)
+//    rdd.collect().foreach(println)
     //关闭连接
     sc.stop()
   }

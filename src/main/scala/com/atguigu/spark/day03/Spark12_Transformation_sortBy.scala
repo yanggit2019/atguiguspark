28b84111b3cdcf380f55f52d60dd71861bcf20b7
yanggit2019 <838067203@qq.com>
2020-09-06 18:24:55 +0800

    sortBy升序

---
+++ src/main/scala/com/atguigu/spark/day03/Spark12_Transformation_sortBy.scala
@@ -0,0 +1,26 @@
+package com.atguigu.spark.day03
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+//重新分区
+object Spark12_Transformation_sortBy {
+  def main(args: Array[String]): Unit = {
+    //2.创建Spark配置文件对象
+    val conf: SparkConf = new SparkConf().setMaster("local[*]").setAppName("Spark12_Transformation_sortBy")
+    //1.创建SparkContext对象
+    val sc = new SparkContext(conf)
+    val rdd: RDD[Int] = sc.makeRDD(List(1,4,3,2))
+    //升序排序
+    val SortedRdd: RDD[Int] = rdd.sortBy(num => num)
+    SortedRdd.collect().foreach(println)
+    
+   
+    
+//    newRdd.collect().foreach(println)
+    
+
+    //关闭连接
+    sc.stop()
+  }
+}

c66e56dc1c317eb03c49a8942e14729c714f282c
yanggit2019 <838067203@qq.com>
2020-09-06 17:33:15 +0800

    去重不指定分区

--- src/main/scala/com/atguigu/spark/day03/Spark10_Transformation_distinct.scala
+++ src/main/scala/com/atguigu/spark/day03/Spark10_Transformation_distinct.scala
@@ -3,21 +3,20 @@ package com.atguigu.spark.day03
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
-//随机抽选
-object Spark09_Transformation_sample {
+//去重
+object Spark10_Transformation_distinct {
   def main(args: Array[String]): Unit = {
     //2.创建Spark配置文件对象
-    val conf: SparkConf = new SparkConf().setMaster("local[*]").setAppName("Spark09_Transformation_sample")
+    val conf: SparkConf = new SparkConf().setMaster("local[*]").setAppName("Spark10_Transformation_distinct")
     //1.创建SparkContext对象
     val sc = new SparkContext(conf)
 //    val rdd: RDD[String] = sc.makeRDD(List("wangqiao","xiaojing", "hanqi", "chengjiang", "xiaohao"))
 //    val newRdd: RDD[String] = rdd.filter(_.contains("xiao"))
 //    
 //    newRdd.collect().foreach(println)
-    val rdd: RDD[Int] = sc.makeRDD(1 to 10)
-//    val newRdd: RDD[Int] = rdd.sample(true, 3)
-    //从rdd中随机抽取一些数
-    val newRdd: RDD[Int] = rdd.sample(false, 0.6)
+    val rdd: RDD[Int] = sc.makeRDD(List(1,2,3,4,5,5,4,3,2,3,6),5)
+    val newRdd: RDD[Int] = rdd.distinct()
+    
     newRdd.collect().foreach(println)
     
 

46c4752af08071dc5c27e655e897fdc709e5873f
yanggit2019 <838067203@qq.com>
2020-09-07 00:42:00 +0800

    leftjoin 连接

--- src/main/scala/com/atguigu/spark/day04/Spark09_Transformation_join.scala
+++ src/main/scala/com/atguigu/spark/day04/Spark09_Transformation_join.scala
@@ -12,7 +12,10 @@ object Spark09_Transformation_join {
     val sc = new SparkContext(conf)
     val rdd1: RDD[(Int, String)] = sc.makeRDD(List((1, "a"), (2, "b"), (3, "c")))
     val rdd2: RDD[(Int, Int)] = sc.makeRDD(List((1, 4), (2, 5), (4,6)))
-    val newRdd: RDD[(Int, (Int, String))] = rdd2.join(rdd1)
+    //join算子相当于内连接,将两个RDD中的key相同的数据匹配,如果key匹配不上,那么数据不关联
+//    val newRdd: RDD[(Int, (Int, String))] = rdd2.join(rdd1)
+//    val newRdd: RDD[(Int, (String, Int))] = rdd1.join(rdd2)
+val newRdd: RDD[(Int, (String, Option[Int]))] = rdd1.leftOuterJoin(rdd2)
     newRdd.collect().foreach(println)
     //关闭连接
     sc.stop()

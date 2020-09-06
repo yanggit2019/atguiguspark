0c49e2ef89f9a68c8d335ccad213da7f4c16fac0
yanggit2019 <838067203@qq.com>
2020-09-06 08:04:35 +0800

    mapPartitionWithIndex 案例

--- src/main/scala/com/atguigu/spark/day03/Spark02_Transformation_mapPartitionsWithIndex.scala
+++ src/main/scala/com/atguigu/spark/day03/Spark02_Transformation_mapPartitionsWithIndex.scala
@@ -9,14 +9,21 @@ object Spark02_Transformation_mapPartitionsWithIndex {
     val conf: SparkConf = new SparkConf().setMaster("local[*]").setAppName("Spark02_Transformation_mapPartitionsWithIndex")
     //1.创建SparkContext对象
     val sc = new SparkContext(conf)
-    val rdd: RDD[Int] = sc.makeRDD(List(1, 2, 3, 4), 2)
+    val rdd: RDD[Int] = sc.makeRDD(List(1, 2, 3, 4,5,6,7,8), 3)
 //    val newRdd: RDD[Int] = rdd.map(_ * 2)
 //    val newRdd: RDD[Int] = rdd.mapPartitions(datas => {
 //      datas.map(_ * 2)
 //    })
     
-    val newRdd: RDD[(Int, Int)] = rdd.mapPartitionsWithIndex((index, datas) => {
-      datas.map((index, _))
+//    val newRdd: RDD[(Int, Int)] = rdd.mapPartitionsWithIndex((index, datas) => {
+//      datas.map((index, _))
+//    })
+    //需求：第二个分区的数据乘以2，其余分区不变
+    val newRdd: RDD[Int] = rdd.mapPartitionsWithIndex((index, datas) => {
+      index match {
+        case 1 => datas.map(_ * 2)
+        case _ => datas
+      }
     })
     newRdd.collect().foreach(println)
     //关闭连接

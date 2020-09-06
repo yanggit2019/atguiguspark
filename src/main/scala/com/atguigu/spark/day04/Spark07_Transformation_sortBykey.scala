f02112feaeb9e111e33e403a610bdc2788479882
yanggit2019 <838067203@qq.com>
2020-09-07 00:08:38 +0800

    sortByKey按照名字升序排序

--- src/main/scala/com/atguigu/spark/day04/Spark07_Transformation_sortBykey.scala
+++ src/main/scala/com/atguigu/spark/day04/Spark07_Transformation_sortBykey.scala
@@ -11,14 +11,32 @@ object Spark07_Transformation_sortBykey {
     //1.创建SparkContext对象
     val sc = new SparkContext(conf)
     //Rdd本身是没有partitionBy这个算子,通过隐式转换动态给KV类型的Rdd扩展的功能
-    //创建rdd
-    val rdd: RDD[(Int, String)] = sc.makeRDD(Array((3, "aa"), (6, "cc"), (2, "bb"), (1, "dd")))
-    //按照key对rdd中元素进行排序
-    val newRdd: RDD[(Int, String)] = rdd.sortByKey(false)
-
-    newRdd.collect().foreach(println)
+//    //创建rdd
+//    val rdd: RDD[(Int, String)] = sc.makeRDD(Array((3, "aa"), (6, "cc"), (2, "bb"), (1, "dd")))
+//    //按照key对rdd中元素进行排序
+//    val newRdd: RDD[(Int, String)] = rdd.sortByKey(false)
+    //如果key为自定义类型,要求必须混入Ordered特质
+val stuList = List((new Student("jingjing", 18), 1),
+  (new Student("bangzhang", 18), 1),
+  (new Student("jingjing", 19), 1),
+  (new Student("luoxiang", 18), 1),
+  (new Student("jingjing", 20), 1)
+)
+    val rdd: RDD[(Student, Int)] = sc.makeRDD(stuList)
+    rdd.sortByKey().collect().foreach(println)
+    
     
     //关闭连接
     sc.stop()
   }
+}
+class Student(var name:String,var age:Int) extends Ordered[Student] with Serializable {
+  //指定比较规则
+  override def compare(that: Student): Int = {
+    //先按照名称排序,如果名称相同的话,再按年龄排序
+    val res: Int = this.name.compareTo(that.name)
+    res
+  }
+
+  override def toString: String = s"Student($name,$age)"
 }

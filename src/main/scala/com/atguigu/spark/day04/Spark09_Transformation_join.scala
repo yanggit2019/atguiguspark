84c6cec72540447134c55d842e453d9583f1ceea
yanggit2019 <838067203@qq.com>
2020-09-07 00:37:28 +0800

    join连接

--- src/main/scala/com/atguigu/spark/day04/Spark09_Transformation_join.scala
+++ src/main/scala/com/atguigu/spark/day04/Spark09_Transformation_join.scala
@@ -10,7 +10,10 @@ object Spark09_Transformation_join {
     val conf: SparkConf = new SparkConf().setMaster("local[*]").setAppName("Spark09_Transformation_join")
     //1.创建SparkContext对象
     val sc = new SparkContext(conf)
-    
+    val rdd1: RDD[(Int, String)] = sc.makeRDD(List((1, "a"), (2, "b"), (3, "c")))
+    val rdd2: RDD[(Int, Int)] = sc.makeRDD(List((1, 4), (2, 5), (4,6)))
+    val newRdd: RDD[(Int, (Int, String))] = rdd2.join(rdd1)
+    newRdd.collect().foreach(println)
     //关闭连接
     sc.stop()
   }

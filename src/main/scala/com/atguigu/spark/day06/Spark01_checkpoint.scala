252b99e2c0d65857d13ed2b4ed2742f28ef1fece
yanggit2019 <838067203@qq.com>
2020-09-08 14:45:10 +0800

    设置检查点checkPoint

--- src/main/scala/com/atguigu/spark/day06/Spark01_checkpoint.scala
+++ src/main/scala/com/atguigu/spark/day06/Spark01_checkpoint.scala
@@ -7,8 +7,12 @@ object Spark01_checkpoint {
   def main(args: Array[String]): Unit = {
     //2.创建Spark配置文件对象
     val conf: SparkConf = new SparkConf().setMaster("local[*]").setAppName("Spark01_CreateRDD_mem")
+    
+    //开发环境，应该将检查点目录设置在hdfs上
     //1.创建SparkContext对象
     val sc = new SparkContext(conf)
+    //设置检查点目录
+    sc.setCheckpointDir("H:\\spark-0105\\output")
     //3.创建rdd
     val rdd: RDD[String] = sc.makeRDD(List("hello bangzhang", "hello jingjing"), 2)
     //4.映射
@@ -21,6 +25,8 @@ object Spark01_checkpoint {
    
     //打印血缘关系
     println(mapRdd.toDebugString)
+    //设置检查点
+    mapRdd.checkpoint()
     //触发行动操作
     mapRdd.collect().foreach(println)
     println("------------------------")

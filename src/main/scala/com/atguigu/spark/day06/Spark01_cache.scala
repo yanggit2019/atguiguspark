01958af3793ffce6388a749d82fe506f5d5c4de4
yanggit2019 <838067203@qq.com>
2020-09-08 11:38:12 +0800

    cache 没加之前

---
+++ src/main/scala/com/atguigu/spark/day06/Spark01_cache.scala
@@ -0,0 +1,34 @@
+package com.atguigu.spark.day06
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+object Spark01_cache {
+  def main(args: Array[String]): Unit = {
+    //2.创建Spark配置文件对象
+    val conf: SparkConf = new SparkConf().setMaster("local[*]").setAppName("Spark01_CreateRDD_mem")
+    //1.创建SparkContext对象
+    val sc = new SparkContext(conf)
+    //3.创建rdd
+    val rdd: RDD[String] = sc.makeRDD(List("hello bangzhang", "hello jingjing"), 2)
+    //4.映射
+    val flatMapRdd: RDD[String] = rdd.flatMap(_.split(" "))
+    val mapRdd: RDD[(String, Int)] = flatMapRdd.map {
+      word => {
+        println("------------------")
+        (word, 1)
+      }
+    }
+    //打印血缘关系
+    println(mapRdd.toDebugString)
+    //触发行动操作
+    mapRdd.collect()
+    println("------------------------")
+    //打印血缘关系
+    println(mapRdd.toDebugString)
+    //触发行动操作
+    mapRdd.collect()
+    //关闭连接
+    sc.stop()
+  }
+}

bb0ec029288861b053ab4c8c5ffe939d7c0ce426
yanggit2019 <838067203@qq.com>
2020-09-10 16:23:30 +0800

    RDD转换成DataFrame

--- src/main/scala/com/atguigu/spark/sql/day01/SparkSQL01_Demo.scala
+++ src/main/scala/com/atguigu/spark/sql/day01/SparkSQL01_Demo.scala
@@ -1,6 +1,7 @@
 package com.atguigu.spark.sql.day01
 
 import org.apache.spark.SparkConf
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
 //演示RDD&DF&DS之间关系以及转换
@@ -10,14 +11,23 @@ object SparkSQL01_Demo {
     val conf = new SparkConf().setAppName("SparkSQL01_Demo").setMaster("local[*]")
     //创建SPARKSQL 执行的入口对象 SparkSession
     val spark: SparkSession = SparkSession.builder().config(conf).getOrCreate()
+    //导入隐式转换依赖
+    //注意:Spark不是包名，也不是类名，是我们创建的入口对象SparkSession的对象名称
+    import spark.implicits._
     //读取json文件创建DataFrame
-    val df: DataFrame = spark.read.json("H:\\spark-0105\\input\\test.json")
+//    val df: DataFrame = spark.read.json("H:\\spark-0105\\input\\test.json")
     //查看df里面的数据
 //    df.show()
     //SQL语法风格
 //    df.createOrReplaceTempView("user")
 //    spark.sql("select * from user").show()
     //DSL风格
-    df.select("name","age").show()
+//    df.select("name","age").show()
+    //RDD----->DataFrame----->DataSet
+    //创建RDD
+    val rdd: RDD[(Int, String, Int)] = spark.sparkContext.makeRDD(List((1, "banzhang", 20), (2, "jingjing", 18), (3, "wangqiang", 30)))
+    //RDD----->DataFrame
+    val df: DataFrame = rdd.toDF("id","name","age")
+    df.show()
   }
 }
